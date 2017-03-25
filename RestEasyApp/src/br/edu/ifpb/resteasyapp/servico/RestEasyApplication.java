4a616bb483d478c4f2eb33cf7b13257bfe210a55
Diego Gama <diegogama.ifpb@gmail.com>
2017-03-25 11:02:44 -0300

    Service by Lorena

--- RestEasyApp/src/br/edu/ifpb/resteasyapp/servico/RestEasyApplication.java
+++ RestEasyApp/src/br/edu/ifpb/resteasyapp/servico/RestEasyApplication.java
@@ -7,7 +7,7 @@ import javax.ws.rs.core.Application;
 
 import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 
-import br.edu.ifpb.resteasyapp.controller.AlunoController;
+import br.edu.ifpb.resteasyapp.controller.FazendaController;
 
 public class RestEasyApplication extends Application {
 
@@ -25,7 +25,7 @@ public class RestEasyApplication extends Application {
 		this.singletons.add(filter);
 		
 		// Controllers disponíveis no serviço.
-		//this.singletons.add(new AlunoController());		
+		this.singletons.add(new FazendaController());		
 	}
 
 	public Set<Class<?>> getClasses() {
