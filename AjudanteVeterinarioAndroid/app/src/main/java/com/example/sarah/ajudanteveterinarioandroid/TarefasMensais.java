f9d1c14a4f6590087ed76bcbbd800ea921aaeddd
012sa <sarahccosta@hotmail.com>
2017-03-30 22:31:27 -0300

    prints e ajustes nos layouts

--- AjudanteVeterinarioAndroid/app/src/main/java/com/example/sarah/ajudanteveterinarioandroid/TarefasMensais.java
+++ AjudanteVeterinarioAndroid/app/src/main/java/com/example/sarah/ajudanteveterinarioandroid/TarefasMensais.java
@@ -9,5 +9,6 @@ public class TarefasMensais extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarefas_mensais);
+
     }
 }
