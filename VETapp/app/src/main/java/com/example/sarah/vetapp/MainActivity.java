01662cb6da8df37a03c471c247dd5eedbca397ee
012sa <sarahccosta@hotmail.com>
2017-04-03 22:20:30 -0300

    VetApp

---
+++ VETapp/app/src/main/java/com/example/sarah/vetapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sarah.vetapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mfazendas = (Button) findViewById(R.id.fazendas);
+        mfazendas.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ListarFazendas.class);
+                startActivity(i);
+            }
+        });
+        Button mtarefasMensais = (Button) findViewById(R.id.tarefasMensais);
+        mtarefasMensais.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, TarefasMensais.class);
+                startActivity(i);
+            }
+        });
+        Button msair = (Button) findViewById(R.id.sair);
+        mfazendas.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                onDestroy();
+            }
+        });
+    }
+}
---
+++ VETapp/app/src/main/java/com/example/sarah/vetapp/TarefasMensais.java
@@ -0,0 +1,14 @@
+package com.example.sarah.vetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TarefasMensais extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarefas_mensais);
+
+    }
+}
