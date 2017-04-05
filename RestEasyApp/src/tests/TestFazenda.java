7029b76d46a56a0cc08271ad14c3d06ac0a80875
Diego Gama <diegogama.ifpb@gmail.com>
2017-04-05 20:21:47 -0300

    Entidades by Lorena
    Entidades, DAOs e Controllers.
    Falta Controle Reprodutivo, métodos de update e delete nos DAOs e
    mapeamento do Hibernate.

--- RestEasyApp/src/tests/TestFazenda.java
+++ RestEasyApp/src/tests/TestFazenda.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import br.edu.ifpb.resteasyapp.entidade.Fazenda;
+import br.edu.ifpb.resteasyapp.entidade.FazendaInteressada;
 import br.edu.ifpb.resteasyapp.hibernate.HibernateUtil;
 import org.hibernate.Session;
 
@@ -14,7 +14,7 @@ public class TestFazenda {
 				Session session	= HibernateUtil.getSessionFactory().openSession();
 				session.beginTransaction();
 
-				Fazenda f = new Fazenda("fazendinha", "representante", "test@test.com", "mensagem");
+				FazendaInteressada f = new FazendaInteressada("fazendinha", "representante", "test@test.com", "mensagem");
 				session.save(f);
 				session.getTransaction().commit();
 	}
