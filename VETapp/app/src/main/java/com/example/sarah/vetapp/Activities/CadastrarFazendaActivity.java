e9666f6f128367dc516260be8c82df78654da92c
012sa <sarahccosta@hotmail.com>
2017-04-04 23:21:27 -0300

    VETapp Retrofit

--- VETapp/app/src/main/java/com/example/sarah/vetapp/Activities/CadastrarFazendaActivity.java
+++ VETapp/app/src/main/java/com/example/sarah/vetapp/Activities/CadastrarFazendaActivity.java
@@ -7,8 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.sarah.vetapp.R;
 import com.example.sarah.vetapp.Entity.Fazenda;
+import com.example.sarah.vetapp.R;
 
 public class CadastrarFazendaActivity extends Activity {
 
--- VETapp/app/src/main/java/com/example/sarah/vetapp/Network/ConnectionServer.java
+++ VETapp/app/src/main/java/com/example/sarah/vetapp/Network/ConnectionServer.java
@@ -9,33 +9,33 @@ import retrofit2.Retrofit;
 
 public class ConnectionServer {
 
-   // private static final String URL_BASES = "http://localhost:8080/RestEasyApp/";
+   private static final String URL_BASES = "http://localhost:8080/RestEasyApp/";
 
 
-    //private static APIService service;
-    //private static ConnectionServer ourInstance = new ConnectionServer();
+    private static APIService service;
+    private static ConnectionServer ourInstance = new ConnectionServer();
 
-    //public static ConnectionServer getInstance() {
-     //   return ourInstance;
-    //}
+    public static ConnectionServer getInstance() {
+       return ourInstance;
+    }
 
-    //public APIService getService() {
-    //    return service;
-    //}
+    public APIService getService() {
+        return service;
+    }
 
-    //private ConnectionServer() {
-    //    updateServiceAdress();
-    //}
+    private ConnectionServer() {
+        updateServiceAdress();
+    }
 
-    //public void updateServiceAdress() {
+    public void updateServiceAdress() {
 
 
 
-      //  Retrofit retrofit = new Retrofit.Builder()
-        //        .baseUrl(URL_BASES)
-          //      .addConverterFactory(GsonConverterFactory.create())
-            //    .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL_BASES)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        //service = retrofit.create(APIService.class);
-    //}
+        service = retrofit.create(APIService.class);
+    }
 }
