5e2681e11581234a99b3a24d24e0f9c91f6e002d
012sa <sarahccosta@hotmail.com>
2017-04-09 09:06:41 -0300

    acho que foi

---
+++ AjudanteVeterinarioAndroid/app/src/main/java/com/example/sarah/ajudanteveterinarioandroid/Activity/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.sarah.ajudanteveterinarioandroid.Activity;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sarah.ajudanteveterinarioandroid.R;
+
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttontarefasmensais = (Button) findViewById(R.id.irTarefa);
+        buttontarefasmensais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent itm = new Intent(MainActivity.this, TarefasMensaisActivity.class);
+                startActivity(itm);
+            }
+        });
+
+        Button bfazendas = (Button) findViewById(R.id.irFazenda);
+        bfazendas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iFarm = new Intent(MainActivity.this, ListarFazendaActivity.class);
+                startActivity(iFarm);
+            }
+        });
+
+        FloatingActionButton saida = (FloatingActionButton) findViewById(R.id.floatingActionButtonSair);
+        saida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ AjudanteVeterinarioAndroid/app/src/main/java/com/example/sarah/ajudanteveterinarioandroid/Activity/TarefasMensaisActivity.java
@@ -0,0 +1,29 @@
+package com.example.sarah.ajudanteveterinarioandroid.Activity;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.sarah.ajudanteveterinarioandroid.R;
+
+public class TarefasMensaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarefas_mensais);
+
+        FloatingActionButton fbvoltarmenu = (FloatingActionButton) findViewById(R.id.floatingActionButtonVoltarMenuTM);
+        fbvoltarmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ivm = new Intent(TarefasMensaisActivity.this, MainActivity.class);
+                startActivity(ivm);
+            }
+        });
+
+    }
+}
